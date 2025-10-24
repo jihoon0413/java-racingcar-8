8ba61be8fa0a5204987675db6a2f4f71936af743
jihoon <wlgnstls0413@naver.com>
2025-10-24 21:30:15 +0900

    feat: 입력 메소드들을 묶어주는 메소드 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,22 +1,44 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.domain.Splitter;
+import racingcar.dto.GameInitInputDto;
 
 public class InputView {
-    public void printInitMessage() {
+
+    private Splitter splitter;
+
+    public InputView() {
+        this.splitter = new Splitter();
+    }
+
+    public GameInitInputDto ask() {
+        printInitMessage();
+        List<String> carNameList = splitter.split(readInput());
+        printAskRepeatCount();
+        int repeatCount = readRepeatCount();
+        return new GameInitInputDto(carNameList, repeatCount);
+    }
+
+    private void printInitMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public void printAskRepeatCount() {
+    private void printAskRepeatCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
 
-    public String readInput() {
+    private String readInput() {
         return Console.readLine();
     }
 
-    public int readRepeatCount() {
-        return Integer.parseInt(Console.readLine());
+    private int readRepeatCount() {
+        try{
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("반복할 횟수는 숫자로 입력해주세요");
+        }
     }
 
 }

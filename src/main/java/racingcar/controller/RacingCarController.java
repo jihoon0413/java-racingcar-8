b6f5af8ca70cc473a044b0e0a81188b30a8ff45f
jihoon <wlgnstls0413@naver.com>
2025-10-26 14:22:15 +0900

    feat: 게임 결과 출력

--- src/main/java/racingcar/controller/RacingCarController.java
+++ src/main/java/racingcar/controller/RacingCarController.java
@@ -4,19 +4,24 @@ import racingcar.dto.GameInitInputDto;
 import racingcar.dto.GameWinnerDto;
 import racingcar.service.RacingCarService;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class RacingCarController {
 
     private final InputView inputView;
     private final RacingCarService service;
+    private final OutputView outputView;
 
     public RacingCarController() {
         this.inputView = new InputView();
         this.service = new RacingCarService();
+        this.outputView = new OutputView();
     }
+
     public void run() {
         GameInitInputDto dto = inputView.ask();
-        GameWinnerDto result =  service.play(dto);
+        GameWinnerDto result = service.play(dto);
+        outputView.printWinners(result);
         // 결과 출력
     }
 
